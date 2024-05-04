5cdc384ff8f7c7d7c97064bc649f1a055c0fcfab
Stefan Pijnacker <stefanpijnacker@gmail.com>
2024-05-04 15:03:29 +0200

    📚 DXC Api helpers/includes added to dependencies

---
+++ Headers/Graphics/DXCommon.h
@@ -0,0 +1,12 @@
+#pragma once
+// Standard header files almost relevant to any DirectX and/or Graphics code file
+
+#include <dxgi1_6.h>
+#include <d3d12.h>
+#include <d3dx12.h>
+
+#include <dxc/dxcapi.h>
+#include <dxc/dxcapi.use.h>
+
+#include <wrl.h>
+using namespace Microsoft::WRL;
