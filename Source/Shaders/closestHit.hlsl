cf553c590f447be09dc538ce805f2bd1c097788b
Stefan Pijnacker <stefanpijnacker@gmail.com>
2024-05-09 18:36:44 +0200

    🌈 Barycentric coordinates lerping colors per vertex

--- Source/Shaders/closestHit.hlsl
+++ Source/Shaders/closestHit.hlsl
@@ -3,5 +3,17 @@
 [shader("closesthit")]
 void ClosestHit(inout HitInfo payload, Attributes attrib)
 {
-    payload.colorAndDistance = float4(1.0f, 0.2f, 0.2f, RayTCurrent());
+    // The input received is a float2 representing barycentric coordinates u & v
+    // where:
+    // x = 1 - u - v
+    // y = u
+    // z = v
+    
+    float3 a = float3(1, 0, 0);
+    float3 b = float3(0, 1, 0);
+    float3 c = float3(0, 0, 1);
+    
+    float3 hit = a * (1.0 - attrib.bary.x - attrib.bary.y) + b * attrib.bary.x + c * attrib.bary.y;
+    
+    payload.colorAndDistance = float4(hit, RayTCurrent());
 }
