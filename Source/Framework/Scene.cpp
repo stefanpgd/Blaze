80c8006407502fe566b75020ff032ea1e8e52807
Stefan Pijnacker <stefanpijnacker@gmail.com>
2024-06-02 13:00:45 +0200

    🧽 Sponza & Flight helmet test models added, together with a updated default scene

--- Source/Framework/Scene.cpp
+++ Source/Framework/Scene.cpp
@@ -4,17 +4,20 @@
 
 Scene::Scene()
 {
-	// TODO: Remove once there is scene control, for now it acts as the default model(s) //
+	AddModel("Assets/Models/FlightHelmet/FlightHelmet.gltf");
+	models[0]->transform.Position = glm::vec3(0.8f, -0.55f, 3.0f);
+	models[0]->transform.Rotation = glm::vec3(0.0f, -30.0f, 0.0f);
+	models[0]->transform.Scale = glm::vec3(2.0f, 2.0f, 2.0f);
+
 	AddModel("Assets/Models/GroundPlane/plane.gltf");
-	models[0]->transform.Position = glm::vec3(0.0f, -0.55f, 0.0f);
-
-	AddModel("Assets/Models/Dragon/dragon.gltf");
-	models[1]->transform.Position = glm::vec3(-1.95f, 0.0f, -2.0f);
-	models[1]->transform.Rotation = glm::vec3(0.0f, 40.0f, 0.0f);
-
-	AddModel("Assets/Models/Bunny/bunny.gltf");
-	models[2]->transform.Position = glm::vec3(0.0f, -0.6f, 0.0f);
+	models[1]->transform.Position = glm::vec3(0.0f, -0.55f, 0.0f);
 	
+	AddModel("Assets/Models/Dragon/dragon.gltf");
+	models[2]->transform.Rotation = glm::vec3(0.0f, 40.0f, 0.0f);
+	
+	AddModel("Assets/Models/Bunny/bunny.gltf");
+	models[3]->transform.Position = glm::vec3(-1.95f, -0.6f, -2.0f);
+
 	// Environment Map //
 	std::string exrPath = "Assets/EXRs/wharf.exr";
 	environmentMap = new EnvironmentMap(exrPath);
