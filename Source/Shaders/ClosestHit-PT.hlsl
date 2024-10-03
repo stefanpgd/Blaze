be606811d160c66c3fb52e5c2f65d766c59992ed
Stefan Pijnacker <stefanpijnacker@gmail.com>
2024-10-03 22:49:21 +0200

    🏭 Reconstructing the PT renderng pipeline into 3 material models

--- Source/Shaders/ClosestHit-PT.hlsl
+++ Source/Shaders/ClosestHit-PT.hlsl
@@ -26,181 +26,38 @@ struct Material
 };
 ConstantBuffer<Material> material : register(b0);
 
-float Fresnel(float3 incoming, float3 normal, float IoR)
+float3 ComputeConductorRadiance(float3 albedo, float3 normal, float3 currentDepth)
 {
-    float cosI = dot(incoming, normal);
-    float n1 = 1.0f;
-    float n2 = IoR;
-
-    if(cosI > 0.0f)
-    {
-        float t = n1;
-        n1 = n2;
-        n2 = t;
-    }
-
-    float sinR = n1 / n2 * sqrt(max(1.0f - cosI * cosI, 0.0f));
-    if(sinR >= 1.0f)
-    {
-		// TIR, aka perfect reflectance, which happens at the exact edges of a surface.
-        return 1.0f;
-    }
-
-    float cosR = sqrt(max(1.0f - sinR * sinR, 0.0f));
-    cosI = abs(cosI);
-
-    float Fp = (n2 * cosI - n1 * cosR) / (n2 * cosI + n1 * cosR);
-    float Fr = (n1 * cosI - n2 * cosR) / (n1 * cosI + n2 * cosR);
-
-    return (Fp * Fp + Fr * Fr) * 0.5f;
+    float3 radiance = 0.0f;
+    
+    float3 intersection = WorldRayOrigin() + WorldRayDirection() * RayTCurrent();
+    float3 direction = reflect(WorldRayDirection(), normal);
+        
+    RayDesc ray;
+    ray.Origin = intersection;
+    ray.Direction = direction;
+    ray.TMin = 0.001f;
+    ray.TMax = 100000;
+        
+    HitInfo reflectLoad;
+    reflectLoad.depth = currentDepth;
+        
+    TraceRay(SceneBVH, RAY_FLAG_NONE, 0xFF, 0, 0, 0, ray, reflectLoad);
+    radiance += reflectLoad.color * albedo;
+    
+    return radiance;
 }
 
-float3 refract2(float3 incoming, float3 normal, float IoR)
+float3 ComputeTransmissionRadiance(float3 albedo, float3 normal, float3 currentDepth)
 {
-    float cosI = dot(incoming, normal);
-    float n1 = 1.0f;
-    float n2 = IoR;
-    float3 norm = normal;
-
-    if(cosI < 0.0f)
+    float3 radiance = 0.0f;
+    
+    float reflectance = Fresnel(WorldRayDirection(), normal, 1.51f);
+    float transmittance = 1.0 - reflectance;
+    float3 intersection = WorldRayOrigin() + WorldRayDirection() * RayTCurrent();
+        
+    if (reflectance > 0.0f)
     {
-		// Going from air into medium
-        cosI = -cosI;
-    }
-    else
-    {
-		// Going from medium back into air
-        float t = n1;
-        n1 = n2;
-        n2 = t;
-        norm = norm * -1.0f;
-    }
-
-    float eta = n1 / n2;
-    float k = 1.0f - eta * eta * (1.0f - cosI * cosI);
-
-    if(k < 0.0f)
-    {
-        return reflect(incoming, norm);
-    }
-
-    float3 a = eta * (incoming + (cosI * norm));
-    float3 b = (norm * -1.0f) * sqrt(k);
-
-    return a + b;
-}
-
-[shader("closesthit")]
-void ClosestHit(inout HitInfo payload, Attributes attrib)
-{
-    int width;
-    int height;
-    diffuseTexture.GetDimensions(width, height);
-    
-    uint vertID = PrimitiveIndex() * 3;
-    
-    Vertex a = VertexData[indices[vertID]];
-    Vertex b = VertexData[indices[vertID + 1]];
-    Vertex c = VertexData[indices[vertID + 2]];
-    
-    float3 baryCoords = float3(1.0f - attrib.bary.x - attrib.bary.y, attrib.bary.x, attrib.bary.y);
-    
-    float3 normal = a.normal * baryCoords.x + b.normal * baryCoords.y + c.normal * baryCoords.z;
-    normal = normalize(mul(ObjectToWorld3x4(), float4(normal, 0.0f)).xyz);
-    
-    float2 uv = frac((a.texCoord0 * baryCoords.x) + (b.texCoord0 * baryCoords.y) + (c.texCoord0 * baryCoords.z));
-    uint2 textureSampleLocation = uint2(uv.x * width, uv.y * height);
-    
-    float3 materialColor = material.color;
-    float alpha = 1.0;
-    
-    if(material.hasDiffuse)
-    {
-        materialColor = diffuseTexture[textureSampleLocation].rgb * material.color;
-        alpha = diffuseTexture[textureSampleLocation].a;
-    }
-    
-    if(material.hasNormal)
-    {
-        float3 tangent = a.tangent * baryCoords.x + b.tangent * baryCoords.y + c.tangent * baryCoords.z;
-        tangent = normalize(mul(ObjectToWorld3x4(), float4(tangent, 0.0f)).xyz);
-        
-        float3 biTangent = cross(normal, tangent);
-        float3x3 TBN = float3x3(tangent, biTangent, normal);
-        
-        float3 n = (normalTexture[textureSampleLocation].rgb * 2.0) - float3(1.0, 1.0, 1.0);
-        normal = normalize(mul(n, TBN));
-    }
-    
-    if(material.hasORM)
-    {
-        // stub //
-    }
-        
-    payload.depth += 1;
-    const uint maxDepth = 6;
-    
-    if(payload.depth >= maxDepth)
-    {
-        payload.color = float3(0.0f, 0.0f, 0.0f);
-        return;
-    }
-
-    float3 colorOutput = float3(0.0f, 0.0f, 0.0f);
-    
-    if(material.isEmissive)
-    {
-        payload.color = materialColor;
-        return;
-    }
-    
-    if(material.isDielectric || alpha < 1.0)
-    {
-        float reflectance = Fresnel(WorldRayDirection(), normal, 1.51f);
-        float transmittance = 1.0 - reflectance;
-        float3 intersection = WorldRayOrigin() + WorldRayDirection() * RayTCurrent();
-        
-        if(reflectance > 0.0f)
-        {
-            float3 direction = reflect(WorldRayDirection(), normal);
-        
-            RayDesc ray;
-            ray.Origin = intersection;
-            ray.Direction = direction;
-            ray.TMin = 0.001f;
-            ray.TMax = 100000;
-        
-            HitInfo reflectLoad;
-            reflectLoad.depth = payload.depth;
-        
-            TraceRay(SceneBVH, RAY_FLAG_NONE, 0xFF, 0, 0, 0, ray, reflectLoad);
-            colorOutput += reflectLoad.color * materialColor * reflectance;
-        }
-        
-        if(transmittance > 0.0f)
-        {
-            float3 direction = refract2(WorldRayDirection(), normal, 1.51f);
-        
-            RayDesc ray;
-            ray.Origin = intersection;
-            ray.Direction = direction;
-            ray.TMin = 0.01f;
-            ray.TMax = 100000;
-        
-            HitInfo refractLoad;
-            refractLoad.depth = payload.depth;
-        
-            TraceRay(SceneBVH, RAY_FLAG_NONE, 0xFF, 0, 0, 0, ray, refractLoad);
-            colorOutput += refractLoad.color * materialColor * transmittance;
-        }
-        
-        payload.color = colorOutput;
-        return;
-    }
-    
-    if(material.specularity > 0.01f)
-    {
-        float3 intersection = WorldRayOrigin() + WorldRayDirection() * RayTCurrent();
         float3 direction = reflect(WorldRayDirection(), normal);
         
         RayDesc ray;
@@ -210,20 +67,49 @@ void ClosestHit(inout HitInfo payload, Attributes attrib)
         ray.TMax = 100000;
         
         HitInfo reflectLoad;
-        reflectLoad.depth = payload.depth;
+        reflectLoad.depth = currentDepth;
         
         TraceRay(SceneBVH, RAY_FLAG_NONE, 0xFF, 0, 0, 0, ray, reflectLoad);
-        colorOutput += reflectLoad.color * material.specularity * materialColor;
+        radiance += reflectLoad.color * albedo * reflectance;
     }
+        
+    if (transmittance > 0.0f)
+    {
+        float3 direction = refract2(WorldRayDirection(), normal, 1.51f);
+        
+        RayDesc ray;
+        ray.Origin = intersection;
+        ray.Direction = direction;
+        ray.TMin = 0.01f;
+        ray.TMax = 100000;
+        
+        HitInfo refractLoad;
+        refractLoad.depth = currentDepth;
+        
+        TraceRay(SceneBVH, RAY_FLAG_NONE, 0xFF, 0, 0, 0, ray, refractLoad);
+        radiance += refractLoad.color * albedo * transmittance;
+    }
+        
+    return radiance;
+}
+
+float3 ComputeDielectricRadiance(float3 albedo, float3 normal, in HitInfo payload)
+{
+    float3 radiance = 0.0f;
     
-    // Surface we hit is 'Diffuse' so we scatter //
-    if(1.0f - material.specularity > 0.01f && alpha >= 0.99)
-    {        
-        float3 BRDF = materialColor / PI; // TODO: Read that article Jacco send about the distribution by Pi
+    const float IOR = 1.519f; // TODO: Move to material struct
     
-        float3 intersection = WorldRayOrigin() + WorldRayDirection() * RayTCurrent();
+    float3 intersection = WorldRayOrigin() + WorldRayDirection() * RayTCurrent();
+    
+    float specularFactor = Fresnel(WorldRayDirection(), normal, IOR);
+    float diffuseFactor = 1.0 - specularFactor;
+    
+    if (diffuseFactor > 0.01)
+    {
+        float3 BRDF = albedo / PI; // TODO: Read that article Jacco send about the distribution by Pi
+    
         float3 direction = RandomUnitVector(payload.seed);
-        if(dot(normal, direction) < 0.0f)
+        if (dot(normal, direction) < 0.0f)
         {
             // Normal is facing inwards //
             direction *= -1.0f;
@@ -237,12 +123,102 @@ void ClosestHit(inout HitInfo payload, Attributes attrib)
         ray.TMin = 0.001f;
         ray.TMax = 100000;
     
-        TraceRay(SceneBVH, RAY_FLAG_NONE, 0xFF, 0, 0, 0, ray, payload);
-        float3 irradiance = payload.color * cosI;
+        HitInfo diffuseLoad;
+        diffuseLoad.depth = payload.depth;
+        
+        TraceRay(SceneBVH, RAY_FLAG_NONE, 0xFF, 0, 0, 0, ray, diffuseLoad);
+        float3 irradiance = diffuseLoad.color * cosI;
     
         float3 diffuse = (PI * 2.0f * BRDF * irradiance) * (1.0f - material.specularity);
-        colorOutput += diffuse;
+        radiance += diffuse;
     }
     
+    if(specularFactor > 0.01)
+    {
+        float3 direction = reflect(WorldRayDirection(), normal);
+        
+        const float roughnessOffset = 0.3f;
+        float3 randomInSphere = 0.0f;
+        randomInSphere.x = RandomInRange(payload.seed, -roughnessOffset, roughnessOffset);
+        randomInSphere.y = RandomInRange(payload.seed, -roughnessOffset, roughnessOffset);
+        randomInSphere.z = RandomInRange(payload.seed, -roughnessOffset, roughnessOffset);
+        
+        direction += randomInSphere;
+        
+        RayDesc ray;
+        ray.Origin = intersection;
+        ray.Direction = direction;
+        ray.TMin = 0.001f;
+        ray.TMax = 100000;
+        
+        HitInfo reflectLoad;
+        reflectLoad.depth = payload.depth;
+        
+        TraceRay(SceneBVH, RAY_FLAG_NONE, 0xFF, 0, 0, 0, ray, reflectLoad);
+        radiance += reflectLoad.color * albedo * specularFactor;
+    }
+    
+    return radiance;
+}
+
+[shader("closesthit")]
+void ClosestHit(inout HitInfo payload, Attributes attrib)
+{
+    // Handle ray-tree depth //
+    payload.depth += 1;
+    const uint maxDepth = 6;
+
+    if (payload.depth >= maxDepth)
+    {
+        payload.color = float3(0.0f, 0.0f, 0.0f);
+        return;
+    }
+    
+    // Vertex Data //
+    uint vertID = PrimitiveIndex() * 3;
+    Vertex a = VertexData[indices[vertID]];
+    Vertex b = VertexData[indices[vertID + 1]];
+    Vertex c = VertexData[indices[vertID + 2]];
+    
+    float3 baryCoords = float3(1.0f - attrib.bary.x - attrib.bary.y, attrib.bary.x, attrib.bary.y);
+    
+    float3 normal = a.normal * baryCoords.x + b.normal * baryCoords.y + c.normal * baryCoords.z;
+    float3 tangent = a.tangent * baryCoords.x + b.tangent * baryCoords.y + c.tangent * baryCoords.z;
+    float2 uv = frac((a.texCoord0 * baryCoords.x) + (b.texCoord0 * baryCoords.y) + (c.texCoord0 * baryCoords.z));
+    
+    normal = normalize(mul(ObjectToWorld3x4(), float4(normal, 0.0f)).xyz);
+    tangent = normalize(mul(ObjectToWorld3x4(), float4(tangent, 0.0f)).xyz);
+    
+    // Texture 
+    int width;
+    int height;
+    diffuseTexture.GetDimensions(width, height);
+    uint2 textureSampleLocation = uint2(uv.x * width, uv.y * height);
+    
+    float3 albedo = material.color;
+    if (material.hasDiffuse)
+    {
+        albedo = diffuseTexture[textureSampleLocation].rgb * material.color;
+    }
+    
+    if (material.hasNormal)
+    {
+        float3 biTangent = cross(normal, tangent);
+        float3x3 TBN = float3x3(tangent, biTangent, normal);
+        
+        float3 n = (normalTexture[textureSampleLocation].rgb * 2.0) - float3(1.0, 1.0, 1.0);
+        normal = normalize(mul(n, TBN));
+    }
+    
+    // Calculate Radiance //
+    float3 colorOutput = float3(0.0f, 0.0f, 0.0f);
+    
+    if (material.isEmissive)
+    {
+        payload.color = albedo;
+        return;
+    }
+    
+    colorOutput = ComputeDielectricRadiance(albedo, normal, payload);
     payload.color = colorOutput;
 }
--- Source/Shaders/Common.hlsl
+++ Source/Shaders/Common.hlsl
@@ -37,3 +37,67 @@ float3 RandomUnitVector(inout uint seed)
     float3 vec = float3(x, y, z);
     return normalize(vec);
 }
+
+float Fresnel(float3 incoming, float3 normal, float IoR)
+{
+    float cosI = dot(incoming, normal);
+    float n1 = 1.0f;
+    float n2 = IoR;
+
+    if (cosI > 0.0f)
+    {
+        float t = n1;
+        n1 = n2;
+        n2 = t;
+    }
+
+    float sinR = n1 / n2 * sqrt(max(1.0f - cosI * cosI, 0.0f));
+    if (sinR >= 1.0f)
+    {
+		// TIR, aka perfect reflectance, which happens at the exact edges of a surface.
+        return 1.0f;
+    }
+
+    float cosR = sqrt(max(1.0f - sinR * sinR, 0.0f));
+    cosI = abs(cosI);
+
+    float Fp = (n2 * cosI - n1 * cosR) / (n2 * cosI + n1 * cosR);
+    float Fr = (n1 * cosI - n2 * cosR) / (n1 * cosI + n2 * cosR);
+
+    return (Fp * Fp + Fr * Fr) * 0.5f;
+}
+
+float3 refract2(float3 incoming, float3 normal, float IoR)
+{
+    float cosI = dot(incoming, normal);
+    float n1 = 1.0f;
+    float n2 = IoR;
+    float3 norm = normal;
+
+    if (cosI < 0.0f)
+    {
+		// Going from air into medium
+        cosI = -cosI;
+    }
+    else
+    {
+		// Going from medium back into air
+        float t = n1;
+        n1 = n2;
+        n2 = t;
+        norm = norm * -1.0f;
+    }
+
+    float eta = n1 / n2;
+    float k = 1.0f - eta * eta * (1.0f - cosI * cosI);
+
+    if (k < 0.0f)
+    {
+        return reflect(incoming, norm);
+    }
+
+    float3 a = eta * (incoming + (cosI * norm));
+    float3 b = (norm * -1.0f) * sqrt(k);
+
+    return a + b;
+}
